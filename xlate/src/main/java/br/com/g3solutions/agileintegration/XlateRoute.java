3da9964f79ca13d0ea93f63306bfce4b9748369c
Marlon Patrick <marlon.patrick@g3solutions.com.br>
2019-09-19 09:03:18 -0300

    Adjust Xlate to convert ExecuteMatchUpdate to a Soap format

--- xlate/src/main/java/br/com/g3solutions/agileintegration/XlateRoute.java
+++ xlate/src/main/java/br/com/g3solutions/agileintegration/XlateRoute.java
@@ -4,17 +4,19 @@ import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.TypeConversionException;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.dataformat.soap.name.ServiceInterfaceStrategy;
 import org.springframework.stereotype.Component;
 
 import com.customer.app.Person;
 import com.sun.mdm.index.webservice.ExecuteMatchUpdate;
+import com.sun.mdm.index.webservice.PersonEJB;
 
 @Component
 public class XlateRoute  extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-    	    	
+
     	from("amqp:queue:deim.in").routeId("XlateRoute")
 			.onException(TypeConversionException.class)
 			.maximumRedeliveries(3)
@@ -27,7 +29,7 @@ public class XlateRoute  extends RouteBuilder {
     	.log(">>> Person XML:\n${body}")
     	.unmarshal().jaxb(Person.class.getPackage().getName())
     	.convertBodyTo(ExecuteMatchUpdate.class)
-    	.marshal().jaxb(Boolean.TRUE)
+    	.marshal().soapjaxb(PersonEJB.class.getPackage().getName(), new ServiceInterfaceStrategy(PersonEJB.class, Boolean.TRUE))
     	.log(">>> ExecuteMatchUpdate XML:\n${body}")
 		.setExchangePattern(ExchangePattern.InOnly)
 		.to("amqp:queue:nextgate.out")
